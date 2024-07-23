7d1bba7f713743508fc6713e0ca665c901de8a62
DanielAdisa <61038143+DanielAdisa@users.noreply.github.com>
2024-07-23 22:51:03 +0100

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 type Props = {}
 
 export default function Projects({}: Props) {
-    const projects = [1,2,3,4,5,6];
+    // const projects = [1,2,3,4,5,6];
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3  className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
         <div className=' relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
-          {projects.map((project, i) => (
+{/*           {projects.map((project, i) => ( */}
             <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                 <Image
                 src={"/IMG_20190908_102805.jpg"} 
@@ -20,10 +20,10 @@ export default function Projects({}: Props) {
                 alt='one'
                 />
               <div>
-                <h4>Case Study {i+1} of {projects.length} : Netflix Clone </h4>
+{/*                 <h4>Case Study {i+1} of {projects.length} : Netflix Clone </h4> */}
               </div>
             </div>
-          ))}
+{/*           ))} */}
         </div>
         <div className=' w-full absolute top-[30%] bg-[#25D366]/10 left-0 h-[500px] -skew-y-12'/>
         <div className=' w-full absolute top-[30%] bg-[#25D366]/10 left-0 h-[500px] skew-y-12'/>
