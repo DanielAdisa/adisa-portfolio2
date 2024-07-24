99b1f9727a21d5ed3aada01878f6e09cb0df6dab
Your Name <you@example.com>
2024-07-24 21:35:35 +0100

    uuu

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -6,14 +6,11 @@ function ContactMe({}: Props) {
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left  
     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center scroll-smooth">
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
             Experience
         </h3>
         <div className=" flex flex-col space-y-10">
-            <h4 className="text-4xl font-semibold text-center">
-                I'm H.I.M, I deliver.
-                <span className=' underline decoration-[#25D366]/50'> Lets Talk.</span> 
-            </h4>
+            <h4 className="text-4xl font-semibold text-center">I'm H.I.M, I deliver. <span className="underline decoration-[#25D366]/50"> Lets Talk.</span> </h4>
 
             <div className=" space-y-3">
                 <div className=" flex items-center space-x-5 justify-center">
