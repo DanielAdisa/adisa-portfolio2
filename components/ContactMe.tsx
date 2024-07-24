2f6b768137004b21fefa9f22619e265225f1be8f
Your Name <you@example.com>
2024-07-24 21:53:36 +0100

    df

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -10,7 +10,7 @@ function ContactMe({}: Props) {
             Experience
         </h3>
         <div className=" flex flex-col space-y-10">
-            <h4 className="text-4xl font-semibold text-center">I am H.I.M, I deliver. <span className="underline decoration-[#25D366]/50"> Lets Talk.</span> </h4>
+            <h4 className="text-4xl font-semibold text-center">I{"'"}m H.I.M, I deliver. <span className="underline decoration-[#25D366]/50"> Lets Talk.</span> </h4>
 
             <div className=" space-y-3">
                 <div className=" flex items-center space-x-5 justify-center">
@@ -20,10 +20,8 @@ function ContactMe({}: Props) {
  
 
                 <div className=" flex items-center space-x-5 justify-center">
-                    <Link href={'https://www.google.com/maps/place/Abuja,+Federal+Capital+Territory/data=!4m2!3m1!1s0x104e745f4cd62fd9:0x53bd17b4a20ea12b?sa=X&ved=1t:242&ictx=111'}>
                     <MapPinIcon className=" text-[#25D366] h-7 w-7 animate-ping"/>
                     <p className="text-2xl">Abuja, Nigeria</p>
-                    </Link>
                 </div>
 
                 <div className=" flex items-center space-x-5 justify-center">
