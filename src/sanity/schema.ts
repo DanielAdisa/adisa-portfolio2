c9e32ef44786abef88ff3c8a4fc57779caecc411
Your Name <you@example.com>
2024-07-27 08:50:42 +0100

    new

--- src/sanity/schema.ts
+++ src/sanity/schema.ts
@@ -4,7 +4,7 @@ import { type SchemaTypeDefinition } from 'sanity'
 import {pageInfo} from './schemaTypes/pageInfo'
 import { skill } from './schemaTypes/skill'
 import { experience } from './schemaTypes/experience'
-import { social } from './schemaTypes/Social'
+import { social } from './schemaTypes/social'
 import { project } from './schemaTypes/project'
 
 
