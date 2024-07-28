7627a87742566c88e22c0467d77d5ee7593ba231
Your Name <you@example.com>
2024-07-28 23:31:29 +0100

    zx

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -7,14 +7,14 @@ const query = groq`
 `
 
 type Data = {
-    socials : Social[]
+    // socials : Social[]
 }
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
   ) {
-    const socials: Social[] = await sanityClient.fetch(query);
-    res.status(200).json({ socials });
+    // const socials: Social[] = await sanityClient.fetch(query);
+    // res.status(200).json({ socials });
   }
   
